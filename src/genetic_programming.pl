58b9233bfcd8e2aa1e4901dfc63b9bcc7ceb9ee8
flowpoint <flowpoint>
2023-05-16 14:47:12 +0200

    add descriptive comments and fix indent

--- src/genetic_programming.pl
+++ src/genetic_programming.pl
@@ -1,25 +1,25 @@
 :- module(genetic_programming, [genetic_programming/3]).
 
-% taskspec is some structure that contains:
-% initialization/initial population
-% selectionop
-% crossoverop
-% mutationop
-% stopcondition
-% costfunction
-
 task([TaskName, Costfn, Initializer, StopCondition]) :-
+    % unique identifier to summarize the task
     TaskName = "Learn String",
+    % cost function describes what the tasks goal is
+    % by assigning a cost value to the "solutions"
     Costfn = _,
+    % initializer describes the initial state the task should be started from
+    % this is part of the task, so the optimizers can be agnostic to the tasks
+    % additionally the task definition might contain realistically an inital 
+    % state to be started from
     Initializer = _,
+    % the stopcondition defines when a task is achieved
     StopCondition = _.
 
 %optimizer(Selectionop, Crossoverop, Mutationop).
 optimizer(_,_,_).
 
- genetic_programming(Task, Optimizer, EvolutionHistory) :-
-     task(Task),
-     Optimizer = _,
-     EvolutionHistory = ["Hello world"].
+genetic_programming(Task, Optimizer, EvolutionHistory) :-
+    task(Task),
+    Optimizer = _,
+    EvolutionHistory = ["Hello world"].
 
     
